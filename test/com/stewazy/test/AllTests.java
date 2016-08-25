7719f4a192f49ffd0d3224a4f99ae514e5c917c8
stewazy <stewazy@gmail.com>
2016-08-25 18:33:37 +0200

    Added comments

--- test/com/stewazy/test/AllTests.java
+++ test/com/stewazy/test/AllTests.java
@@ -4,6 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * Run all tests
+ * @author Stevan Mihajlov
+ *
+ */
+
 @RunWith(Suite.class)
 @SuiteClasses({ OperationTest.class, KeyboardInputTest.class })
 public class AllTests {
--- test/com/stewazy/test/KeyboardInputTest.java
+++ test/com/stewazy/test/KeyboardInputTest.java
@@ -9,23 +9,32 @@ import org.junit.Test;
 
 import com.stewazy.main.KeyboardInput;
 
-
+/**
+ * Testing methods in KeyboardInput.java
+ * @author Stevan Mihajlov
+ *
+ */
 public class KeyboardInputTest {
-	
 
+	/**
+	 * Testing getting double value
+	 */
 	@Test
 	public void testGetDouble() {
 		InputStream anyInputStream = new ByteArrayInputStream("2".getBytes());
 		KeyboardInput input = new KeyboardInput(anyInputStream);
 		assertEquals(2, input.getDouble(),0);
 	}
-	
+
+	/**
+	 * Testing getting char value
+	 */
 	@Test
 	public void testGetChar() {
 		InputStream anyInputStream = new ByteArrayInputStream("c".getBytes());
 		KeyboardInput input = new KeyboardInput(anyInputStream);
 		assertEquals('c',input.getChar()); 
 	}
-	
+
 
 }
